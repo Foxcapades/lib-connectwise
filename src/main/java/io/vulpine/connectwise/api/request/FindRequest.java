975fb3a4a0b5f3bc2db557cf76ec833582a351ab
Ellie Harper <elliefops@gmail.com>
2016-08-04 15:09:07 -0400

    updated generics on find requests

--- src/main/java/io/vulpine/connectwise/api/request/FindRequest.java
+++ src/main/java/io/vulpine/connectwise/api/request/FindRequest.java
@@ -47,7 +47,7 @@ abstract public class FindRequest < T > extends CwRequest < T >
     return orderBy;
   }
 
-  public FindRequest setOrderBy ( final String orderBy )
+  public FindRequest < T > setOrderBy ( final String orderBy )
   {
     this.orderBy = orderBy;
     return this;
@@ -58,7 +58,7 @@ abstract public class FindRequest < T > extends CwRequest < T >
     return limit;
   }
 
-  public FindRequest setLimit ( final int limit )
+  public FindRequest < T > setLimit ( final int limit )
   {
     this.limit = limit;
     return this;
@@ -69,7 +69,7 @@ abstract public class FindRequest < T > extends CwRequest < T >
     return skip;
   }
 
-  public FindRequest setSkip ( final int skip )
+  public FindRequest < T >  setSkip ( final int skip )
   {
     this.skip = skip;
     return this;
@@ -80,7 +80,7 @@ abstract public class FindRequest < T > extends CwRequest < T >
     return returnFields;
   }
 
-  public FindRequest setReturnFields ( final List < String > returnFields )
+  public FindRequest < T > setReturnFields ( final List < String > returnFields )
   {
     this.returnFields = returnFields;
     return this;
