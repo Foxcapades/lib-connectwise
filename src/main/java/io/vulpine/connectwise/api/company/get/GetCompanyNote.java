5fa806751de7a55df7a160167383f69b61816d9a
Elizabeth <eharper@excelmicro.com>
2016-08-22 17:12:10 -0400

    Added annotations

--- src/main/java/io/vulpine/connectwise/api/company/get/GetCompanyNote.java
+++ src/main/java/io/vulpine/connectwise/api/company/get/GetCompanyNote.java
@@ -1,5 +1,8 @@
 package io.vulpine.connectwise.api.company.get;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement( localName = "GetCompanyNote" )
 public class GetCompanyNote
 {
 }
