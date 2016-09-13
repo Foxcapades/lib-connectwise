99711e3f4d8ea95c4513367d9542befb28a2f803
Ellie Harper <elliefops@gmail.com>
2016-09-12 20:01:10 -0400

    Issue #18 cont'd

--- src/main/java/io/vulpine/connectwise/api/endpoints/managedDevice/UpdateManagedWorkstations.java
+++ src/main/java/io/vulpine/connectwise/api/endpoints/managedDevice/UpdateManagedWorkstations.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.vulpine.connectwise.api.endpoints.managedDevice;
+package io.vulpine.connectwise.api.endpoints.manageddevice;
 
 public class UpdateManagedWorkstations
 {
